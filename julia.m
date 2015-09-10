ab676e30b220072fc2f29e589cd316b83b5e7871
Roman Juranek <ijuranek@fit.vutbr.cz>
2015-09-10 14:21:16 +0200

    Better parallelization and more memory effecient (setting hight number of samples will no longer exhaust the memory).

--- julia.m
+++ julia.m
@@ -1,4 +1,4 @@
-function [I1,rs,is] = julia(sz, rect, c, cdir, nSamples, maxIter, fun)
+function [I,rs,is] = julia(sz, rect, c, cdir, nSamples, maxIter, fun)
 % Render julia fractal
 %
 %   [I,r,i] = julia(sz, rect, c, cdir, nSamples, maxIter, [fun])
@@ -25,31 +25,31 @@ if nargin<7, fun = @(z,c) z.^2+c; end;
 % Viewport into Julia space
 rs = single(linspace(rect(1),rect(2),sz(1)));
 is = single(linspace(rect(3),rect(4),sz(2))); 
-[r,i] = meshgrid(rs,is); Z = complex(r,i); % Coordinates of individual pixels
+[r,i] = meshgrid(rs,is); % Coordinates of individual pixels
 d = (rs(2)-rs(1))/4; % Smoothing extent depends on pixel size
 
 % Generate z and c for every pixel and sample
-Z = repmat(Z,1,1,nSamples);
-Z = Z + d*complex(randn(size(Z),'single'),randn(size(Z),'single'));
-C = repmat(c, size(Z)); C = C + cdir * randn(size(C));
+Z = complex(r,i); C = repmat(c,size(Z)); I = zeros(sz(1), sz(2), nSamples);
 
-% Split to workers
-Z1 = num2cell(Z,1); C1 = num2cell(C,1);
-
-% Do it!
-parfor i = 1:numel(Z1),I{i}=arrayfun(@(z,c)renderElement(z,c,maxIter,fun),Z1{i},C1{i});end;
-
-% Get the resulting image
-I = reshape(I,size(Z1)); I1 = cell(1,size(I,3));
-for i = 1:size(I,3),I1{i} = [I{:,:,i}]; end;
-I1 = cat(3,I1{:}); I1 = mean(I1,3);
+batch = 4;
+for s = 1:batch:nSamples
+    smps = s:min(s+batch-1,nSamples); ns = length(smps);
+    Z1 = repmat(Z,1,1,ns); Z1 = Z1 + d*complex(randn(size(Z1),'single'),randn(size(Z1),'single'));
+    C1 = repmat(C,1,1,ns); C1 = C1 + cdir * randn(size(C1));    
+    Z2 = num2cell(Z1,1); C2 = num2cell(C1,1); % Split to workers
+    ims = cell(1,numel(Z2));
+    parfor i = 1:numel(Z2),ims{i}=renderElement(Z2{i},C2{i},maxIter,fun);end; % Do the job
+    I(:,:,smps) = reshape(cell2mat(ims), sz(1), sz(2), []); % Get the resulting image
+end
 
 end
 
 function n = renderElement(z, c, maxIter, fun)
-n = maxIter;
-for j = 1:maxIter % Main loop
-    z = fun(z, c);
-    if abs(z)>4,n=j;break;end;
-end;
+n = repmat(maxIter,size(z));
+for i = 1:numel(z)
+    for j = 1:maxIter % Main loop
+        z(i) = fun(z(i), c(i));
+        if abs(z(i))>4,n(i)=j;break;end;
+    end;
+end
 end
